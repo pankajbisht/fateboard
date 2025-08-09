417aed3bf387c4f6cd738bd923158ec51c62419f
pankaj bisht <pankajbisht031@gmail.com>
2025-08-09 23:46:20 +0530

    Trigger GitHub Pages rebuild

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ function App() {
   return <>
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<EditorPage />}>
+        <Route path="/fate" element={<EditorPage />}>
         </Route>
       </Routes>
     </BrowserRouter>
