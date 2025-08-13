eee198a7fce16a5cc4c721f836621fc922168a70
pankaj bisht <pankajbisht031@gmail.com>
2025-08-14 01:07:53 +0530

    bring back

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 
 // https://vite.dev/config/
-export default defineConfig({
-  base: "/",
+export default defineConfig(({ mode }) => ({
+  base: mode === 'gh-pages' ? '/fate/' : '/',
   plugins: [react(), tailwindcss()],
-})
+}))
